b7629aceb9118d9609e761db722de3eb085e2985
APTist24 <58297571+APTist24@users.noreply.github.com>
2024-08-21 16:36:49 +0000

    save

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
+import {Address} from "ton"
 
-// write your NFT miner here
+async function main () 
+{
+  const wallet = Address.parse('0QAah3Fl6C1fkFQ8cuY4f3yBHGPMSprjRScZtUajw1aT-u5F');
+  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+}
+
+main()
